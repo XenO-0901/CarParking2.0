88f5e59b61c94956e6b5f5333cb3cc5b97285384
XenO-0901 <113492105+XenO-0901@users.noreply.github.com>
2023-04-14 18:25:04 +0800

    Updating password on Database
    Test lang kung nagbabago na password sa database. Baka kaya lang laging Bad Request sa'kin ay dahil patay database, gano'n. Napa-check ko rin kay ChatGPT at nag-update s'ya ng ibang lines. Mukhang okay naman... daw.

--- Website parking/USER/app.py
+++ Website parking/USER/app.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template, session, request, g, redirect, url_for
 import os
+import sqlite3
+
+#if you want to use the save info in the database the url must be http://localhost:5001/login
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+DATABASE = 'database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+        db.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL, phone TEXT NOT NULL, platenumber TEXT NOT NULL)')
+        db.commit()
+    return db
+
 
 @app.before_request
 def before_request():
@@ -11,16 +26,9 @@ def before_request():
     if 'user' in session:
         g.user = session['user']
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        session.pop('user', None)
-
-        if request.form['password'] == 'password':
-            session['user'] = request.form['username']
-            return redirect(url_for('userview'))
-        
-    return render_template('LoginPage.html')
+    return render_template('loading.html')
 
 @app.route('/userview')
 def userview():
@@ -28,32 +36,60 @@ def userview():
         return render_template('index.html', user=session['user'])
     return redirect(url_for('index'))
 
-@app.route('/omg')
-def omg():
-    return render_template('index.html')
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        cur = db.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+        user = cur.fetchone()
+
+        if user:
+            session['user'] = user['username']
+            return redirect(url_for('userview'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+
+    # Handle GET requests
     return render_template('login.html')
+
+
+@app.route('/forgotpass', methods={'GET', 'POST'})
+def forgotpass():
+    username = request.form['username']
+    password = request.form['password']
+    db = get_db()
+    #cur = db.execute('SELECT * FROM users WHERE username = ?', (username))
+    cur = db.execute('UPDATE users SET password = ? WHERE username = ?', (password, username))
+    db.commit()
+
+    if cur.rowcount > 0:
+        session['user'] = username
+        return redirect(url_for('userview'))
+    else:
+        return render_template('forgotpass.html', error='Invalid password')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Handle form submission
-        name = request.form['name']
-        platenumber = request.form['plateno']
+        username = request.form['name']
+        platenumber = request.form['platenumber']
         email = request.form['email']
-        phone = request.form['contact']
+        phone = request.form['phone']
         password = request.form['password']
-        conpassword = request.form['conpassword']
 
-        # Do something with the form data (e.g. save to database)
+        db = get_db()
 
-        return "Thanks for signing up, " + name + "!"
+        try:
+            db.execute('INSERT INTO users (username, platenumber, email, phone, password) VALUES (?, ?, ?, ?, ?)', (username, platenumber, email, phone, password))
+            db.commit()
+            session['user'] = username
+            return redirect(url_for('login'))
+        except sqlite3.IntegrityError:
+            return render_template('signup.html', error='Username already exists')
 
-    # If the request method is GET, render the signup page
     return render_template('signup.html')
-
 
 if __name__ == '__main__':
     app.run(port=5001)
